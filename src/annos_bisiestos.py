9a1cf9b4649d65f3e59ed20de69cb0a31f507fb0
Yenifer Leon <neutronwar34@gmail.com>
2024-09-07 21:20:56 -0500

    Cambio años bisiestos

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,13 +1,20 @@
 def evaluar(anno):
-    if anno <1582:
-        if anno % 4 == 0:
-            return f"{anno} es bisiesto"
-        else: 
-            return f"{anno} no es bisiesto"
-    else: 
-        if (anno % 4 == 0 and anno % 100 != 0) or (anno % 400 ==0):
-            return f"{anno} es bisiesto"
-        else: f"{anno} no es bisiesto"
+    if (anno < 1582):
+        if (anno % 4 == 0):
+            return str(anno) + " es bisiesto"
+        else:
+            return str(anno) + " no es bisiesto"
+    else:
+        if (anno % 4 == 0):
+            if (anno % 100 == 0):
+                if (anno % 400 == 0):
+                    return str(anno) + " es bisiesto"
+                else:
+                    return str(anno) + " no es bisiesto"
+            else:
+                return str(anno) + " es bisiesto"
+        else:
+            return str(anno) + " no es bisiesto"
 
 if __name__ == '__main__':
     print("Año:", end="")
